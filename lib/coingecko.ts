db069a37ade2faf9d5015a55acb626fdfd2d43a8
Bundler <dev@bundledao.org>
2021-07-27 23:45:44 -0700

    Add charting support

---
+++ lib/coingecko.ts
@@ -0,0 +1,21 @@
+const request = async (url: string) => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const response = await fetch(url, options);
+
+    if (response.status !== 200) {
+        return response.status;
+    }
+
+    const result = await response.json();
+    return result;
+}
+
+export const fetchChartData = async (id: string, days: number = 1) => {
+    return request(`https://api.coingecko.com/api/v3/coins/${id.toLowerCase()}/market_chart?vs_currency=usd&days=${days}`);
+}
